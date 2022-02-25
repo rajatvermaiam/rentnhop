361a226839e6e1d1832738de83fde99c9cb5c180
Rajat <rajatvermamprs@gmail.com>
2022-02-25 11:36:19 +0530

    Bugs

--- routes/admin.php
+++ routes/admin.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\Adminv\admin\AdminController;
+use App\Http\Controllers\Adminv\Admin\AdminController;
 use App\Http\Controllers\Adminv\Admin\BlogCategeoryController;
 use App\Http\Controllers\Adminv\Admin\BlogPostController;
-use App\Http\Controllers\Adminv\admin\CitiesController;
+use App\Http\Controllers\Adminv\Admin\CitiesController;
 use App\Http\Controllers\Adminv\Admin\CouponController;
-use App\Http\Controllers\Adminv\admin\LocationController;
-use App\Http\Controllers\Adminv\admin\UserController;
-use App\Http\Controllers\Adminv\admin\VehicleController;
+use App\Http\Controllers\Adminv\Admin\LocationController;
+use App\Http\Controllers\Adminv\Admin\UserController;
+use App\Http\Controllers\Adminv\Admin\VehicleController;
 
 Route::get('dashboard', [AdminController::class, 'dashboard']);
 
